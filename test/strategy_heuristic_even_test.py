ee5f4f716e530684b49835c39f7f7468cc1401f9
jonas@valfridsson.net <jonas@valfridsson.net>
2019-03-06 11:42:19 +0100

    Fixed the fuck out of #74!

--- test/strategy_heuristic_even_test.py
+++ test/strategy_heuristic_even_test.py
@@ -2,14 +2,14 @@ import strategy_heuristic.strategy_heuristic as sh
 print("Starting evaluation")
 sh.evaluate(
     render=True,
-    robots=85,
-    spawn=200,
+    robots=84,
+    spawn=100,
     capacity=1,
-    shelve_length=2,
+    shelve_length=4,
     shelve_width=10,
     shelve_height=10,
     steps=10000,
-    periodicity_lower=300,
-    periodicity_upper=500,
+    periodicity_lower=100,
+    periodicity_upper=700,
     collect=True,
     even=True)
